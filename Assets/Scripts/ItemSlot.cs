202190727aaced722e6d71dc52c6cf4f915e8ad5
DESKTOP-BRTMTMV\Nathan <natemi1010@gmail.com>
2019-06-24 15:38:55 +0100

    End of video 2

--- Assets/Scripts/ItemSlot.cs
+++ Assets/Scripts/ItemSlot.cs
@@ -10,5 +10,9 @@
             this.item = item;
             this.quantity = quantity;
         }
+
+        public static bool operator ==(ItemSlot a, ItemSlot b) { return a.Equals(b); }
+
+        public static bool operator !=(ItemSlot a, ItemSlot b) { return !a.Equals(b); }
     }
 }
