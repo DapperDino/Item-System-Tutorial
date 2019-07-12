dd8c22b0704180994aff7bd7735fdb567a04af86
DESKTOP-BRTMTMV\Nathan <natemi1010@gmail.com>
2019-07-12 10:33:00 +0100

    End of video 9

---
+++ Assets/Scripts/Items/Ammunition.cs
@@ -0,0 +1,25 @@
+﻿using DapperDino.Items.Inventories;
+using System.Text;
+using UnityEngine;
+
+namespace DapperDino.Items
+{
+    [CreateAssetMenu(fileName = "New Ammunition", menuName = "Items/Ammunition")]
+    public class Ammunition : InventoryItem
+    {
+        [SerializeField] private GameObject ammunitionPrefab = null;
+
+        public GameObject AmmunitionPrefab => ammunitionPrefab;
+
+        public override string GetInfoDisplayText()
+        {
+            StringBuilder builder = new StringBuilder();
+
+            builder.Append(Rarity.Name).AppendLine();
+            builder.Append("Max Stack: ").Append(MaxStack).AppendLine();
+            builder.Append("Sell Price: ").Append(SellPrice).Append(" Gold");
+
+            return builder.ToString();
+        }
+    }
+}
---
+++ Assets/Scripts/Utilities/ToggleActiveWithKeyPress.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+namespace DapperDino.Utilities
+{
+    public class ToggleActiveWithKeyPress : MonoBehaviour
+    {
+        [SerializeField] private KeyCode keyCode = KeyCode.None;
+        [SerializeField] private GameObject objectToToggle = null;
+
+        private void Update()
+        {
+            if (Input.GetKeyDown(keyCode))
+            {
+                objectToToggle.SetActive(!objectToToggle.activeSelf);
+            }
+        }
+    }
+}
